24cde6fece741869177d53c260bb3d1a893a37c9
Edward S <38728269+etnava@users.noreply.github.com>
2020-04-24 11:02:41 +1000

    Getting a list of all the crypto currency

--- src/main/java/com/cryptocurrencyApp/CryptocurrencyAppApplication.java
+++ src/main/java/com/cryptocurrencyApp/CryptocurrencyAppApplication.java
@@ -2,12 +2,16 @@ package com.cryptocurrencyApp;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.model.Cryptocurrency;
@@ -17,16 +21,21 @@ import com.model.Cryptocurrency;
 public class CryptocurrencyAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CryptocurrencyAppApplication.class, args);
-		
+		//SpringApplication.run(CryptocurrencyAppApplication.class, args);
+
 		ObjectMapper objectMapper = new ObjectMapper();
-		Cryptocurrency cryptocurrency = new Cryptocurrency("bitcoin");
 		try {
-			objectMapper.writeValue(new File("target/cryptocurrency.json"), cryptocurrency);
-		} catch (JsonGenerationException e) {
+			List<Cryptocurrency> listCrypto = objectMapper.readValue(new URL("https://api.coingecko.com/api/v3/coins/list"), new TypeReference<List<Cryptocurrency>>() {});
+			for (Cryptocurrency c : listCrypto) {
+				System.out.println(c.getId());
+			}
+		} catch (JsonParseException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (JsonMappingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (MalformedURLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
@@ -34,5 +43,5 @@ public class CryptocurrencyAppApplication {
 			e.printStackTrace();
 		}
 	}
-	
+
 }
--- src/main/java/com/model/Cryptocurrency.java
+++ src/main/java/com/model/Cryptocurrency.java
@@ -2,15 +2,42 @@ package com.model;
 
 public class Cryptocurrency {
 
-	private final String id;
+	private String id;
+	private String symbol;
+	private String name;
 
-	public Cryptocurrency(String id) {
+	public Cryptocurrency() {
+		super();
+	}
+
+	public Cryptocurrency(String id, String symbol, String name) {
 		this.id = id;
-
+		this.symbol = symbol;
+		this.name = name;
 	}
 
 	public String getId() {
 		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getSymbol() {
+		return symbol;
+	}
+
+	public void setSymbol(String symbol) {
+		this.symbol = symbol;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
 	}
 
 }

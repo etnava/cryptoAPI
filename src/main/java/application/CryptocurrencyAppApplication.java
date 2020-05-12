acf310b58e8457b7afcb6f6a7c12b40b22bba0ec
Edward S <38728269+etnava@users.noreply.github.com>
2020-05-12 16:22:13 +1000

    Removed old controller and converter Service

--- src/main/java/application/CryptocurrencyAppApplication.java
+++ src/main/java/application/CryptocurrencyAppApplication.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.client.RestTemplate;
 
-@ComponentScan(basePackages = { "com.controller" })
+@ComponentScan(basePackages = { "controller" })
 @SpringBootApplication
 public class CryptocurrencyAppApplication {
 
